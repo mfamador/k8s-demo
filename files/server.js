664405c38d46351705b3e5fabc2ec9b602136827
Marco Amador <amador.marco@gmail.com>
2019-09-19 10:06:10 +0100

    Add resource demo files

---
+++ files/server.js
@@ -0,0 +1,10 @@
+var http = require('http');
+const os = require('os');
+
+var handleRequest = function(request, response) {
+  console.log('Received request for URL: ' + request.url);
+  response.writeHead(200);
+  response.end("You've hit " + os.hostname() + " version:1\n");
+};
+var www = http.createServer(handleRequest);
+www.listen(8080);
