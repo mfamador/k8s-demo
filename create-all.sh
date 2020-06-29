468b3d4f0b0b73487fcb93e205b92ef49ad03023
Marco Amador <amador.marco@gmail.com>
2020-06-29 22:12:01 +0100

    add ingress

--- create-all.sh
+++ create-all.sh
@@ -2,12 +2,13 @@
 
 kubectl create -f 01-deployment.yaml
 kubectl create -f 02-service.yaml
-kubectl create -f 03-configmap.yaml
-kubectl create -f 04-deployment-configmap.yaml
-kubectl create -f 05-secret.yaml
-kubectl create -f 06-deployment-secret.yaml
-kubectl create -f 07-job.yaml
-kubectl create -f 08-cronjob.yaml
-kubectl create -f 09-daemonset.yaml
-kubectl create -f 10-service-ds.yaml
+kubectl create -f 03-service.yaml
+kubectl create -f 04-configmap.yaml
+kubectl create -f 05-deployment-configmap.yaml
+kubectl create -f 06secret.yaml
+kubectl create -f 07-deployment-secret.yaml
+kubectl create -f 08-job.yaml
+kubectl create -f 09-cronjob.yaml
+kubectl create -f 10-daemonset.yaml
+kubectl create -f 11-service-ds.yaml
 
--- delete-all.sh
+++ delete-all.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
-kubectl delete all --all -n default
-
-kubectl delete secret test-secret
-kubectl delete configmap configs
-
+kubectl delete -f 01-deployment.yaml
+kubectl delete -f 02-service.yaml
+kubectl delete -f 03-service.yaml
+kubectl delete -f 04-configmap.yaml
+kubectl delete -f 05-deployment-configmap.yaml
+kubectl delete -f 06secret.yaml
+kubectl delete -f 07-deployment-secret.yaml
+kubectl delete -f 08-job.yaml
+kubectl delete -f 09-cronjob.yaml
+kubectl delete -f 10-daemonset.yaml
+kubectl delete -f 11-service-ds.yaml
