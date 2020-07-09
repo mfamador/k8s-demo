0762304325855dcc558560c7c83fb63cb81dd833
Marco Amador <amador.marco@gmail.com>
2020-07-09 17:48:31 +0100

    create all resources

--- create-all.sh
+++ create-all.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
-kubectl create -f 01-deployment.yaml
-kubectl create -f 02-service.yaml
-kubectl create -f 03-ingress.yaml
-kubectl create -f 04-configmap.yaml
-kubectl create -f 05-deployment-configmap.yaml
-kubectl create -f 06secret.yaml
-kubectl create -f 07-deployment-secret.yaml
-kubectl create -f 08-job.yaml
-kubectl create -f 09-cronjob.yaml
-kubectl create -f 10-daemonset.yaml
-kubectl create -f 11-service-ds.yaml
+kubectl apply -f 01-deployment.yaml
+kubectl apply -f 02-service.yaml
+kubectl apply -f 03-ingress.yaml
+kubectl apply -f 04-configmap.yaml
+kubectl apply -f 05-deployment-configmap.yaml
+kubectl apply -f 06secret.yaml
+kubectl apply -f 07-deployment-secret.yaml
+kubectl apply -f 08-job.yaml
+kubectl apply -f 09-cronjob.yaml
+kubectl apply -f 10-daemonset.yaml
+kubectl apply -f 11-service-ds.yaml
+kubectl apply -f 12-dashboard-admin-user.yaml
 
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0/aio/deploy/recommended.yaml
