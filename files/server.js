2f7d6d719890a8660a3caf46e02aa691724435cb
Marco Amador <amador.marco@gmail.com>
2022-01-13 12:25:19 +0000

    chore: bump version

--- files/server.js
+++ files/server.js
@@ -4,7 +4,7 @@ const os = require('os');
 var handleRequest = function(request, response) {
   console.log('Received request for URL: ' + request.url);
   response.writeHead(200);
-  response.end("You've hit a critical app " + os.hostname() + " version:1.0.6 \n");
+  response.end("You've hit a critical app " + os.hostname() + " version:1.0.0 \n");
 };
 
 var www = http.createServer(handleRequest);
