282db3d2340211c1f74666ccada46c7d3bc06db5
Marco Amador <amador.marco@gmail.com>
2022-01-22 15:32:34 +0000

    chore: server response
    Signed-off-by: Marco Amador <amador.marco@gmail.com>

--- files/server.js
+++ files/server.js
@@ -2,9 +2,12 @@ var http = require('http');
 const os = require('os');
 
 var handleRequest = function(request, response) {
-  console.log('Received request for URL: ' + request.url);
-  response.writeHead(200);
-  response.end("You've hit a critical app " + os.hostname() + " version:1.0.0 \n");
+    console.log('Received request for URL: ' + request.url);
+
+    var version = 'version 1.0.0'
+
+    response.writeHead(200);
+    response.end("You've hit a critical app " + os.hostname() + " " + version + " \n");
 };
 
 var www = http.createServer(handleRequest);
