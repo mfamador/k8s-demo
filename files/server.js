535052cb98ca65a519e5482ea941be514d6c146d
Marco Amador <amador.marco@gmail.com>
2022-06-05 16:18:53 +0100

    chore: add scripts to build critical app

--- files/server.js
+++ files/server.js
@@ -4,11 +4,91 @@ const os = require('os');
 var handleRequest = function(request, response) {
     console.log('Received request for URL: ' + request.url);
 
-    var version = '0.9.2'
+    var version = '1.0.0'
 
-    response.writeHead(200);
-    response.end("You've hit a critical app (" + os.hostname() + ") version:" + version + " :=) \n");
+    const msgs = new Map();
+    const codes = new Map();
+    
+    codes.set('1.0.0', 200)
+    codes.set('1.1.0', 200)
+    codes.set('1.1.1', 500)
+    codes.set('2.0.0', 200)
+    codes.set('2.1.0', 200)
+    codes.set('2.1.1', 500)
+
+    ver = '1.0.0'
+    msgs.set(ver, `
+    -----------------------------------------
+   < critical app version ` + version + ` returned ` + codes.get(ver) + ` >
+    -----------------------------------------
+    `)
+   
+    ver = '1.1.0'
+    msgs.set(ver, `
+    -----------------------------------------
+   < critical app version ` + version + ` returned ` + codes.get(ver) + ` >
+    -----------------------------------------
+            \\   ^__^
+             \\  (oo)\_______
+                (__)\\       )\\/\\
+                    ||----w |
+                    ||     ||
+    `)
+   
+    ver = '1.1.1'
+    msgs.set(ver, `
+    -----------------------------------------
+   < critical app version ` + version + ` returned ` + codes.get(ver) + ` >   ¯\\_(ツ)_/¯ 
+    -----------------------------------------
+            \\   ^__^
+             \\  (xx)\_______
+                (^^)\\       )\\/\\
+                 >===----w ==<
+    `)
+
+    ver = '2.0.0'
+    msgs.set(ver, `
+    -----------------------------------------
+   < critical app version ` + version + ` returned ` + codes.get(ver) + ` >
+    -----------------------------------------
+            \\    ,-^-.
+            \\    !oYo!
+            \\   /./=\\.\\______
+                   ##        )\\/\\
+                    ||-----w||
+                    ||      ||
+    `)
+
+    ver = '2.1.0'
+    msgs.set(ver, `
+    -----------------------------------------
+   < critical app version ` + version + ` returned ` + codes.get(ver) + ` >
+    -----------------------------------------
+            \\    ,-^-.
+            \\    !oYo!
+            \\   /./=\\.\\______
+                   ##        )\\/\\
+                    ||-----w||
+                    ||      ||
+    `)
+
+    ver = '2.1.1'
+    msgs.set(ver, `
+    -----------------------------------------
+   < critical app version ` + version + ` returned ` + codes.get(ver) + ` >   ¯\\_(ツ)_/¯ 
+    -----------------------------------------
+            \\    ,-^-.
+            \\    !xYx!
+            \\   /./=\\.\\______
+                   ##        )\\/\\
+                >==-----w==<
+    `)
+    
+    response.writeHead(codes.get(version));
+    response.end(msgs.get(version));
 };
 
 var www = http.createServer(handleRequest);
 www.listen(8000);
+
+//   ¯\\_(ツ)_/¯ 
